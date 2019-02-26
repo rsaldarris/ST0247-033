aafa7bc552ad1f9809f454844853e1d8586891c0
rsaldarris <37939454+rsaldarris@users.noreply.github.com>
2019-02-26 15:11:30 -0500

    Create Taller5.java

---
+++ talleres/taller05/Taller5.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+/**
+ * Clase en la cual se implementan los metodos del Taller 5
+ * 
+ * @author Mauricio Toro, Camilo Paez
+ */
+public class Taller5 {
+
+
+	/**
+	* Metodo que dado un grafo y un numero m, se asigna un color
+	* a cada nodo, de manera que dos nodos adyacentes no poseean el mismo color
+	* @param g grafo dado 
+	* @param m numero de colores
+	* @return true si es posible, false de lo contrario
+	*/
+	public static boolean mColoring(Digraph g, int m) {
+		
+	}
+
+	/**
+	* Metodo que dado un grafo y un vertice v, intenta asignar un color
+	* al nodo, de manera que dos nodos adyacentes no poseean el mismo color
+	* @param g grafo dado 
+	* @param m numero de colores
+	* @param v vertice 
+	* @param colors conjunto de colores
+	* @return true si es posible, false de lo contrario
+	*/
+	private static boolean mColoring(Digraph g, int v, int[] colors, int m) {
+		
+	}
+
+	/**
+	* Metodo que dado un grafo y un vertice v, intenta asignar un color colors en la 
+	* posicion c al nodo v, de manera que dos nodos adyacentes no poseean el mismo color
+	* @param g grafo dado 
+	* @param c indice de colores
+	* @param v vertice 
+	* @param colors conjunto de colores
+	* @return true si es posible, false de lo contrario
+	*/
+	private static boolean isSafe(Digraph g, int v, int[] colors, int c) {
+		
+	}
+
+	
+
+}
