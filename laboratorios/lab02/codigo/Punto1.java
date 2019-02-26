43ca9ed5a4e7d21bb13282540d07cc65c96832f4
rsaldarris <37939454+rsaldarris@users.noreply.github.com>
2019-02-26 16:54:48 -0500

    Update Punto1.java

--- laboratorios/lab02/codigo/Punto1.java
+++ laboratorios/lab02/codigo/Punto1.java
@@ -1,1 +1,37 @@
 
+ public static boolean hayCaminoDFS(Digraph g, int v) {
+        boolean[] visitados = new boolean[g.size()];
+        return hayCamino(g, v, visitados,e);
+ }
+
+private static boolean hayCamino(Diagraph g, int v,boolean[] visitado, int e){
+  boolean fin = true;
+  boolean correcto=false;
+  
+  ArrayList<Integer> proximos=g.getSuccesors(e);
+  for(int i=0;i<visitado.length();i++){
+    if(visitado[i]==false && i != v){
+      fin=false;
+      break;
+    }
+  } 
+  
+  for(int i=0;i<proximos.length();i++){
+    int proxVer=proximos.get(i);
+    if(proxVer==v && fin){
+      return true;
+    }
+    if(visitado[proxVer]==false && proxVer != v){
+      visitado[proxVer]=true;
+      correcto=hayCamino(g,v,visitado,proxVer);
+      if(!correcto){
+        visitado[proxVer]=false;
+      }
+    }
+  }
+  if(correcto){
+    return true;
+  }
+  return false;
+  
+}
