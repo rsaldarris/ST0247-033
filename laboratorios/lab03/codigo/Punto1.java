5eead93055dc464398147e470dea9f7cfd6a44b0
rsaldarris <37939454+rsaldarris@users.noreply.github.com>
2019-03-25 16:31:08 -0500

    Update Punto1.java

--- laboratorios/lab03/codigo/Punto1.java
+++ laboratorios/lab03/codigo/Punto1.java
@@ -1,1 +1,37 @@
-
+import java.util.ArrayList;
+import java.util.Stack;
+/**
+ * Write a description of class ShortWayProf here.
+ *
+ * @author Carlos Mesa,  Ricardo Saldarriaga, Benjamin De La Torre.
+ * @version 24/03/19
+ */
+public class ShortWayProf {    
+    public static ArrayList<Integer> ThereWayProf(Digraph g, int end, int start) {
+        boolean[] visit = new boolean[g.size()];
+        Stack<Integer> pile = new Stack<Integer>();
+        ArrayList<Integer> way = new ArrayList<Integer>();
+        ThereWayProf(g,end,start,visit, pile);
+        way.add(start);
+        while( !pile.isEmpty() ) {
+            way.add( pile.pop() );
+        }
+        return way;
+    }
+    
+       public static boolean ThereWayProf(Digraph g, boolean[] visit, int objective, int node, Stack<Integer> c) {
+        ArrayList<Integer> successors = g.getSuccessors(node);
+        visit[node] = true;
+        if( successors != null ) {
+            for( Integer successor : successors ) {              
+                if( (objective || ThereWayProf(g, visit, objective, successor, c) == successor) && !visit[successor] ) {      
+                    c.push( successor );
+                    return true;
+                }
+            }
+        }
+        else {
+          return false;
+        }
+    }
+}
