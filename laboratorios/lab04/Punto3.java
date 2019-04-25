565dc1ba117ca917bc2652e518347c6a4c4df46a
rsaldarris <37939454+rsaldarris@users.noreply.github.com>
2019-04-25 13:23:19 -0500

    Create Punto3.java

---
+++ laboratorios/lab04/Punto3.java
@@ -0,0 +1,11 @@
+Punto 3 laboratorio 4
+Ricardo Saldarriaga
+Carlos Daniel Mesa
+Benjamin de la Torre
+
+3.1 Se usa el algortimo del vecino mas cercano, usando un arraylist para guardar los sucesores del grafo junto al arreglo de booleanos que permite verificar si se ha visitado un nodo. Luego se revisa si el costo entre el nodo inicial y el sucesor es menor al mínimo, entonces se genera un nuevo costo mínimo y se sigue revisando.
+3.2 Se necesita tener un grafo completo porque de lo contrario van a existir nodos inalcanzables o no visitados entonces el algoritmo voraz no asegurará una óptima solución.
+3.3 Se puede usar para los domicilios visitando los nodos en los que se va a entregar un producto.
+3.4 La única estructura d datos utilizada fueron los arreglos para representar las rutas disponibles. El algoritmo clasifica los arreglos con las rutas en un orden ascendente basado en la duración. La ruta con menor duración en la mañana es emparejada con la ruta de la tarde de mayor duración. 
+3.5 O (N log N)
+3.6 N es el numero de rutas definidas en el problema dado.
