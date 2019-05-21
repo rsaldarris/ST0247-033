5e91aa06c7e795f0d1fd33520a5fb582be917bcf
rsaldarris <37939454+rsaldarris@users.noreply.github.com>
2019-05-21 15:58:42 -0500

    Create Camino.java

---
+++ proyecto/EntregaFinal/CodigoJava/Camino.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoeda2;
+
+/*
+ * Clase usada para crear un ArrayList que se pueda ordenar segun su camino
+ * 
+ */
+public class Camino{
+  int id,peso;
+  Camino(int id,int caminoa){
+    this.peso=caminoa;
+    this.id=id;
+  }
+}
